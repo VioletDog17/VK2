c29401be422a71a8f555b5e030e5c1830a977a84
denis <denisgrigo2004@gmail.com>
2024-02-06 20:31:15 +0300

    Try to fix tests

--- src/main/kotlin/Attachments.kt
+++ src/main/kotlin/Attachments.kt
@@ -6,11 +6,7 @@ interface Attachments {
 
 class PhotoAttachments(
     override val type: String = "photo",
-    val peculiarities: Photo = Photo(
-        1,
-        1,
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.wikipedia.org%2Fwiki%2F%25D0%25A0%25D0%25B8%25D0%25BA%25D1%2580%25D0%25BE%25D0%25BB%25D0%25BB%25D0%25B8%25D0%25BD%25D0%25B3&psig=AOvVaw2DtV_Ht6yr3ugw-atO-HKz&ust=1706735828986000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPC-7MmEhoQDFQAAAAAdAAAAABAJ"
-    )
+    val peculiarities: Photo
 ) : Attachments
 
 class Photo(
@@ -24,7 +20,7 @@ class Photo(
 
 class VideoAttachments(
     override val type: String = "video",
-    val peculiarities: Video = Video(1, 1, "RickRoll", 213, "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    val peculiarities: Video
 ) : Attachments
 
 class Video(
@@ -39,13 +35,7 @@ class Video(
 
 class AudioAttachments(
     override val type: String = "audio",
-    val peculiarities: Audio = Audio(
-        1,
-        1,
-        "RickRoll",
-        213,
-        "https://zvukipro.com/pesnya/2907-pesnja-rik-roll-v-raznyh-variantah-rick-roll.html"
-    )
+    val peculiarities: Audio
 ) : Attachments
 
 class Audio(
